47ca4575c25bd335e437f562dc78efdac5cfa050
RaphaelUngerFH <202548@fhwn.ac.at>
2024-11-24 08:49:22 +0100

    Update index.test.js

--- Lab2/test/index.test.js
+++ Lab2/test/index.test.js
@@ -3,7 +3,7 @@ const app = require('../index'); // Adjust the path as necessary
 
 describe('GET /hello', () => {
   it('should return Hello World!', async () => {
-    const response = await request(app).get('/');
+    const response = await request(app).get('/hello');
     expect(response.status).toBe(200);
     expect(response.text).toBe('Hello World!');
   });
