4ed90c3ea27f58989cae9b996be7c10c112d4603
RaphaelUngerFH <202548@fhwn.ac.at>
2024-11-25 10:25:38 +0100

    Remove app insights

--- Lab2/api/index.js
+++ Lab2/api/index.js
@@ -1,12 +1,3 @@
-const appInsights = require('applicationinsights');
-appInsights.setup(process.env.APPINSIGHTS_CONNECTION_STRING)
-  .setAutoCollectRequests(true)
-  .setAutoCollectPerformance(true)
-  .setAutoCollectExceptions(true)
-  .setAutoCollectDependencies(true)
-  .setAutoDependencyCorrelation(true)
-  .start();
-
 const { app } = require('@azure/functions');
 
 app.setup({
