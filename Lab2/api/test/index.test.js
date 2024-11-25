16a2047cc000d66a43b6c265e8976e0405d659c8
RaphaelUngerFH <202548@fhwn.ac.at>
2024-11-25 08:16:38 +0100

    Fix test

--- Lab2/api/test/index.test.js
+++ Lab2/api/test/index.test.js
@@ -1,9 +1,9 @@
 const request = require('supertest');
 const app = require('../index'); // Adjust the path as necessary
 
-describe('GET /hello', () => {
+describe('GET /api/hello', () => {
   it('should return Hello World!', async () => {
-    const response = await request(app).get('/hello');
+    const response = await request(app).get('/api/hello');
     expect(response.status).toBe(200);
     expect(response.text).toBe('Hello World!');
   });
